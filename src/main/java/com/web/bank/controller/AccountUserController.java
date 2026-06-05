983813d9710560fd2c9b9eea434d1e43d862e029
shaiksadhik5786 <shaiksadhik940@gmail.com>
2026-06-05 10:25:21 +0530

    change @req Mapping(/bank)

--- src/main/java/com/web/bank/controller/AccountUserController.java
+++ src/main/java/com/web/bank/controller/AccountUserController.java
@@ -20,7 +20,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
-@RequestMapping("/bank")
 public class AccountUserController {
 
 	@Autowired
